e0595045f6a241ce4522a00ad29f3077fb6cb797
lgh <269145366@qq.com>
2020-08-05 00:38:18 +0800

    miaosha配置

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -1,7 +1,12 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration   //将app的启动类当成一个自动化可以支持配置的bean，自动加载
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})   //将app的启动类当成一个自动化可以支持配置的bean，自动加载
 @RestController    //实现springmvc的controller
+@MapperScan("com.miaoshaproject.dao")  //把对应的dao放置到对应的路径下
 
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "hello world!";
+        UserDO userDo = userDOMapper.selectByPrimaryKey(1);
+        if (userDo == null){
+            return "用户不存在";
+        }else {
+            return userDo.getName();
+        }
     }
     public static void main( String[] args )
     {
